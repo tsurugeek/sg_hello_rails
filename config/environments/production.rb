d2c3cc22b3922ce38c414cee237972e835f1350e
Junya Kemmochi <github@tsurugeek.com>
2018-11-15 21:42:14 +0900

    change smtp settings from gmail to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,16 +93,28 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # for email
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { :protocol => 'https', :host => 'shrouded-mesa-89105.herokuapp.com' }
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   # domain:               'smtp.gmail.com',
+  #   user_name:            ENV['GMAIL_USER'],
+  #   password:             ENV['GMAIL_PASSWD'],
+  #   authentication:       'login',
+  #   enable_starttls_auto: true  }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :protocol => 'https', :host => 'shrouded-mesa-89105.herokuapp.com' }
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    # domain:               'smtp.gmail.com',
-    user_name:            ENV['GMAIL_USER'],
-    password:             ENV['GMAIL_PASSWD'],
-    authentication:       'login',
-    enable_starttls_auto: true  }
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'shrouded-mesa-89105.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
